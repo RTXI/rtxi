8e9eaa6857c78ed81b3565cf1c865b7c89acfb98
Jon Bettencourt <yapatel@gatech.edu>
2009-04-27 15:22:24 +0000

    Added a barebones CppUnit testing suite.

---
+++ testsuite/main.cpp
@@ -0,0 +1,12 @@
+#include <cppunit/TextOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int main(void) {
+    CppUnit::TextUi::TestRunner runner;
+
+    runner.setOutputter(new CppUnit::TextOutputter(&runner.result(),std::cerr));
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+    
+    return runner.run() ? 0 : 1;
+}
