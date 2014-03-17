c64f4a8eae887df81936c1f8e80ac89985db27f1
Yogi <yapatel@gatech.edu>
2014-03-17 18:24:41 -0400

    updating minor string concatenations

--- plugins/dynamo_model_loader/dynamo_model_loader.cpp
+++ plugins/dynamo_model_loader/dynamo_model_loader.cpp
@@ -1,24 +1,22 @@
 /*
- Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
+	 Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+	 This program is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 3 of the License, or
+	 (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+	 This program is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 You should have received a copy of the GNU General Public License
+	 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
 #include <debug.h>
-
-#define _GNU_SOURCE
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
@@ -28,6 +26,7 @@
 #include <main_window.h>
 #include <dynamo_model_loader.h>
 
+#define _GNU_SOURCE
 #define MODEL_MAKEFILE_PATH    "/usr/local/share/rtxi/Makefile.dynamo_compile"
 #define MAKE_CMD_PREFIX        "make -f "
 #define MODEL_SOURCE_SUFFIX    ".dynamo"
@@ -49,25 +48,29 @@ DynamoModelLoader::DynamoModelLoader(void) {
 
 	// add recently used modules to the menu
 	QSettings userprefs;
-	userprefs.setPath("RTXI.org", "RTXI", QSettings::UserScope);
-	QStringList entries = userprefs.entryList("/recentFileList");
+	userprefs.setPath(QSettings::NativeFormat, QSettings::UserScope, "RTXI");
+	userprefs.beginGroup("/recentFileList");
+	QStringList entries = userprefs.childKeys();
+	userprefs.endGroup();
 	int numRecentFiles = entries.size();
 	QString listmodule;
 	QString text;
 	for (int i = 0; i < std::min(numRecentFiles-2,10); ++i) {
-		listmodule = userprefs.readEntry("/recentFileList/" + entries[i]);
+		listmodule = userprefs.value("/recentFileList/" + entries[i]).toString();
 		text = tr("&%1 %2").arg(i).arg(listmodule);
 		menuID = MainWindow::getInstance()->createModuleMenuItem(text,this,SLOT(load_recent(int)));
 		MainWindow::getInstance()->setModuleMenuItemParameter(menuID, i);
 	}
 
 	// add recently used settings files to the menu
-	entries = userprefs.entryList("/recentSettingsList");
+	userprefs.beginGroup("/recentSettingsList");
+	entries = userprefs.childKeys();
+	userprefs.endGroup();
 	numRecentFiles = entries.size()-1;
 	for (int i = 0; i < std::min(numRecentFiles,10); ++i) {
-		listmodule = userprefs.readEntry("/recentSettingsList/" + entries[i]);
+		listmodule = userprefs.value("/recentSettingsList/" + entries[i]).toString();
 		text = tr("&%1 %2").arg(i).arg(listmodule);
-		menuID = MainWindow::getInstance()->createFileMenuItem(text,this,SLOT(load_setting(int)));
+		menuID = MainWindow::getInstance()->createFileMenuItem(text.toStdString(),this,SLOT(load_setting(int)));
 		MainWindow::getInstance()->setFileMenuItemParameter(menuID, i);
 	}
 
@@ -91,7 +94,7 @@ int DynamoModelLoader::set_model_makefile_path(char *s) {
 	return 0;
 }
 
-void DynamoModelLoader::load(char *srcpath) {
+void DynamoModelLoader::load(char* srcpath) {
 	int status;
 	char *path;
 
@@ -109,12 +112,12 @@ void DynamoModelLoader::load(char *srcpath) {
 
 	QString cmd = QString("cd %1; %2 %3 %4%5") .arg(getcwd(NULL, 0)) .arg(
 			MAKE_CMD_PREFIX) .arg(model_makefile_path) .arg(model_name) .arg(
-			MODEL_SUFFIX);
+				MODEL_SUFFIX);
 
 	QString module_name = QString("%1%2") .arg(model_name) .arg(MODEL_SUFFIX);
 
 	DEBUG_MSG("about to compile model with command %s\n", cmd.toAscii());
-	status = CmdLine::getInstance()->execute(cmd.toAscii());
+	status = CmdLine::getInstance()->execute(cmd.toStdString());
 
 	if (status != 0) {
 		ERROR_MSG("build process error\n");
@@ -129,31 +132,29 @@ void DynamoModelLoader::load(char *srcpath) {
 
 void DynamoModelLoader::load_dialog(void) {
 	QSettings userprefs;
-	userprefs.setPath("RTXI.org", "RTXI", QSettings::UserScope);
+	userprefs.setPath(QSettings::NativeFormat, QSettings::UserScope, "RTXI");
 	//QString settingsDir = userprefs.readEntry("/dirs/dynamomodels", getenv("HOME"));
 
-	QString file_name = QFileDialog::getOpenFileName(userprefs.readEntry("/dirs/dynamomodels", getenv("HOME")),
-			"Dynamo Models (*" MODEL_SOURCE_SUFFIX
-			");;All (*.*)", MainWindow::getInstance());
-	//QString file_name = QFileDialog::getOpenFileName(QString::null,"Dynamo Models (*" MODEL_SOURCE_SUFFIX \
-						      ");;All (*.*)",MainWindow::getInstance());
+	QString file_name = QFileDialog::getOpenFileName(MainWindow::getInstance(), userprefs.value("/dirs/dynamomodels", getenv("HOME")).toString(),
+			"Dynamo Models (*" MODEL_SOURCE_SUFFIX ");;All (*.*)"); //, MainWindow::getInstance());
+	/*QString file_name = QFileDialog::getOpenFileName(QString::null,"Dynamo Models (*" MODEL_SOURCE_SUFFIX \
+		");;All (*.*)",MainWindow::getInstance());*/
 
-	load((char *) file_name.toLatin1());
+	load((char *) file_name.toStdString().c_str());
 }
-
 
 void DynamoModelLoader::load_recent(int i) {
 	QSettings userprefs;
-	userprefs.setPath("RTXI.org", "RTXI", QSettings::UserScope);
-	QString filename = userprefs.readEntry("/recentFileList/"+QString::number(i));
+	userprefs.setPath(QSettings::NativeFormat, QSettings::UserScope, "RTXI");
+	QString filename = userprefs.value("/recentFileList/"+QString::number(i)).toString();
 	Plugin::Manager::getInstance()->load(filename.toLatin1());
 }
 
 void DynamoModelLoader::load_setting(int i) {
 	QSettings userprefs;
-	userprefs.setPath("RTXI.org", "RTXI", QSettings::UserScope);
-	QString filename = userprefs.readEntry("/recentSettingsList/"+QString::number(i));
-	printf("Loading settings file: %s\n",filename.toLatin1());
+	userprefs.setPath(QSettings::NativeFormat, QSettings::UserScope, "RTXI");
+	QString filename = userprefs.value("/recentSettingsList/"+QString::number(i)).toString();
+	printf("Loading settings file: %s\n",filename.toStdString().c_str());
 	Settings::Manager::getInstance()->load(filename.toLatin1());
 }
 
