cb3c57a57d719746af6461b3b7af58415bb992b2
Yogi <y.patel@me.com>
2014-03-18 11:32:00 -0400

    minor

--- scripts/install_rtxi.sh
+++ scripts/install_rtxi.sh
@@ -59,7 +59,7 @@ if [ $kernel -eq "1" ]; then
 elif [ $kernel -eq "2" ]; then
 	./configure --enable-rtai --enable-comedi
 elif [ $kernel -eq "3" ]; then
-	./configure --enable-posix --enable-analogy
+	./configure --enable-posix --enable-analogy --disable-comedi
 else
 	echo "Invalid configuration."
 	exit 1
