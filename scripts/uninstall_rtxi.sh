c9a909e04ddc6faef028b62350dfb753ec71b275
Yogi <y.patel@me.com>
2014-04-27 15:52:31 -0400

    adding uninstall script

---
+++ scripts/uninstall_rtxi.sh
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#	Created by Yogi Patel <yapatel@gatech.edu> 2014.1.31
+#
+
+#!/bin/bash
+
+cd ../
+
+# Uninstall rtxi
+sudo make uninstall
+sudo make clean
+sudo rm -rf /usr/local/lib/rtxi
+
+if [ $? -eq 0 ]; then
+	echo "----->RTXI intallation successful. Reboot may be required."
+else
+	echo "----->RTXI installation failed."
+	exit
+fi
+
+echo "----->Type '"sudo rtxi"' to start RTXI. Happy Sciencing!"
+echo "----->Please email help@rtxi.org with any questions/help requests."
+echo "----->Script developed/last modified by Yogi Patel <yapatel@gatech.edu> on May 2014."
