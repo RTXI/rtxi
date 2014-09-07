79d6f9d040d5fec85238b998f160ae77149a5b8f
Yogi <y.patel@me.com>
2014-09-07 12:40:40 -0400

    xenomai now automatically locks application memory; skins don't need to have the option passed to them during user space compile

--- scripts/install_rt_kernel.sh
+++ scripts/install_rt_kernel.sh
@@ -123,7 +123,7 @@ fi
 # Install user libraries
 echo  "----->Installing user libraries"
 cd $build_root
-$xenomai_root/configure --enable-shared --enable-smp --enable-posix-auto-mlockall --enable-dlopen-skins --enable-x86-sep
+$xenomai_root/configure --enable-shared --enable-smp --enable-dlopen-skins --enable-x86-sep
 make
 sudo make install
 
