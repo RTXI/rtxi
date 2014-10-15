cadf92631c7265d89c5b12ec93fd5a7f54c62987
Ansel <aesthapus@gmail.com>
2014-10-15 17:30:19 -0400

    fixed error in runninstat

--- plugins/include/runningstat.cpp
+++ plugins/include/runningstat.cpp
@@ -1,4 +1,5 @@
 #include "runningstat.h"
+#include <rt.h>
 
 // class constructor
 RunningStat::RunningStat() : m_n(0) {}
@@ -10,14 +11,24 @@ void RunningStat::clear() {
 }
 
 void RunningStat::push(double x) {
+<<<<<<< HEAD
+  double p = RT::System::getInstance()->getPeriod();
   m_n++;
+
+=======
+  m_n++;
+>>>>>>> df956023e03a0f8748eba28d4cd28a6143e24cb5
   if (m_n == 1) {
       m_oldM = x;
       m_oldS = 0.0;
   } else {
       m_newM = m_oldM + (x - m_oldM) / m_n;
 //		m_newS = (m_oldS*(m_n-1)/m_n) + (x-m_newM)*(x-m_newM)/(m_n-1); 
+<<<<<<< HEAD
+		m_newS = (m_n-1.0)/m_n*m_oldS + (x-p)*(x-p)/(m_n);
+=======
 		m_newS = (m_n-1.0)/m_n*m_oldS + (x-p)*(x-p)/(m_n)
+>>>>>>> df956023e03a0f8748eba28d4cd28a6143e24cb5
       m_oldM = m_newM;
       m_oldS = m_newS;
     }
