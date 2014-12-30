fdf17bf3f512bdd5ac4b23ac591936060873eba8
Ansel <aesthapus@gmail.com>
2014-12-30 15:48:58 -0500

    edited the install script, made an svg of the icon because the original was a huge png, and edited the rtxi.desktop item so that it works

--- scripts/install_rtxi.sh
+++ scripts/install_rtxi.sh
@@ -68,7 +68,7 @@ fi
 
 echo "----->Putting things into place."
 sudo cp -f libtool /usr/local/lib/rtxi/
-sudo cp -f scripts/RTXI-icon.png /usr/local/lib/rtxi/
+sudo cp -f scripts/RTXI-icon.svg /usr/local/lib/rtxi/
 sudo cp -f scripts/rtxi.desktop /usr/share/applications/
 sudo cp -f /usr/share/applications/rtxi.desktop ~/Desktop/
 sudo chmod +x ~/Desktop/rtxi.desktop
