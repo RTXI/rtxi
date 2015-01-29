a6813ff9f41ab36ea069f8b67c695847942c3e50
Ansel <aesthapus@gmail.com>
2015-01-29 16:37:16 -0500

    forgot to enable systemd service

--- scripts/install_rtxi.sh
+++ scripts/install_rtxi.sh
@@ -78,7 +78,8 @@ sudo cp -f rtxi.conf /etc/
 sudo cp -f /usr/xenomai/sbin/analogy_config /usr/sbin/
 
 if [ $(lsb_release -sc) == "jessie" ]; then
-	sudo cp -f .scripts/rtxi_load_analogy.service /etc/systemd/system/
+	sudo cp -f ./scripts/rtxi_load_analogy.service /etc/systemd/system/
+	sudo systemctl enable rtxi_load_analogy.service
 else
 	sudo cp -f ./scripts/rtxi_load_analogy /etc/init.d/
 	sudo update-rc.d rtxi_load_analogy defaults
