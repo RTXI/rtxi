43eec1df0040b76cd9ea8324e09665cc520868bf
Ansel <aesthapus@gmail.com>
2015-04-03 12:02:43 -0400

    updated intentation of runningstat files

--- deps/rtxi_includes/runningstat.cpp
+++ deps/rtxi_includes/runningstat.cpp
@@ -7,37 +7,37 @@ RunningStat::RunningStat() : m_n(0) {}
 RunningStat::~RunningStat() {}
 
 void RunningStat::clear() {
-  m_n = 0;
+	m_n = 0;
 }
 
 void RunningStat::push(double x) {
-  double p = RT::System::getInstance()->getPeriod(); // gets period in nanoseconds
-  m_n++;
+	double p = RT::System::getInstance()->getPeriod(); // gets period in nanoseconds
+	m_n++;
 
-  if (m_n == 1) {
-      m_oldM = x;
-      m_oldS = 0.0;
-  } else {
-    m_newM = m_oldM + (x - m_oldM) / m_n;
-//	 m_newS = (m_oldS*(m_n-1)/m_n) + (x-m_newM)*(x-m_newM)/(m_n-1); //Sample variance
-    m_newS = (m_n-1.0)/m_n*m_oldS + (1.0/m_n)*(x-p)*(x-p); //Variance relative to rt period
-    m_oldM = m_newM;
-    m_oldS = m_newS;
-  }
+	if (m_n == 1) {
+		m_oldM = x;
+		m_oldS = 0.0;
+	} else {
+		m_newM = m_oldM + (x - m_oldM) / m_n;
+//		m_newS = (m_oldS*(m_n-1)/m_n) + (x-m_newM)*(x-m_newM)/(m_n-1); //Sample variance
+		m_newS = (m_n-1.0)/m_n*m_oldS + (1.0/m_n)*(x-p)*(x-p); //Variance relative to rt period
+		m_oldM = m_newM;
+		m_oldS = m_newS;
+	}
 }
 
 int RunningStat::numValues() const {
-  return m_n;
+	return m_n;
 }
 
 double RunningStat::mean() const {
-  return (m_n > 0) ? m_newM : 0.0;
+	return (m_n > 0) ? m_newM : 0.0;
 }
 
 double RunningStat::var() const {
-  return ((m_n > 1) ? m_newS : 0.0);
+	return ((m_n > 1) ? m_newS : 0.0);
 }
 
 double RunningStat::std() const {
-  return sqrt(var());
+	return sqrt(var());
 }
--- deps/rtxi_includes/runningstat.h
+++ deps/rtxi_includes/runningstat.h
@@ -6,19 +6,19 @@
 #include <math.h>
 
 class RunningStat {
-public:
-  RunningStat();
-  ~RunningStat();
+	public:
+		RunningStat();
+		~RunningStat();
 
-  void clear();
-  void push(double x);
-  int numValues() const;
-  double mean() const;
-  double var() const;
-  double std() const;
+		void clear();
+		void push(double x);
+		int numValues() const;
+		double mean() const;
+		double var() const;
+		double std() const;
 
-private:
-  int m_n;
-  double m_oldM, m_newM, m_oldS, m_newS;
+	private:
+		int m_n;
+		double m_oldM, m_newM, m_oldS, m_newS;
 };
 
