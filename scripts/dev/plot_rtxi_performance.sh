3785684c8417da21d92cdc496b6a7d0089070037
anselg <aesthapus@gmail.com>
2015-04-07 15:32:35 -0400

    need to treat GRAPHICS_DRIVER as a string in if statement, otherwise it'll cause errors

--- scripts/dev/plot_rtxi_performance.sh
+++ scripts/dev/plot_rtxi_performance.sh
@@ -90,7 +90,7 @@ GRAPHICS_CARD=$(lspci | grep VGA | uniq | cut -d":" -f3 | sed 's/ \+/ /g' | sed 
 GRAPHICS_DRIVER=$(sudo lshw -c display | grep "configuration: driver" | cut -d":" -f2 | cut -d"=" -f2 | cut -d" " -f1 | sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
 
 # For nouveau, sometimes, lshw doesn't show that it's loaded but lsmod does. 
-if [ $GRAPHICS_DRIVER == "" ]; then
+if [ "$GRAPHICS_DRIVER" == "" ]; then
 	GRAPHICS_DRIVER=$(lsmod | grep video | sed 's/  */ /g' | cut -d" " -f4)
 fi
 DAQ=$(lspci | grep National | cut -d":" -f3 | sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
