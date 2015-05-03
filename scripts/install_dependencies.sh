87f12c3c724af20d806852ad33c2fd1d7b362e29
Yogi <y.patel@me.com>
2015-05-02 23:13:39 -0400

    minor addition to deps

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -39,7 +39,7 @@ sudo apt-get -y install kernel-package
 sudo apt-get -y install fakeroot crash kexec-tools makedumpfile kernel-wedge
 sudo apt-get -y build-dep linux
 sudo apt-get -y install git-core libncurses5 libncurses5-dev libelf-dev binutils-dev libgsl0-dev vim stress libboost-dev
-sudo apt-get -y install libqt5svg5 libqt5opengl5 libqt5gui5 libqt5core5a libqt5xml5
+sudo apt-get -y install libqt5svg5 libqt5opengl5 libqt5gui5 libqt5core5a libqt5xml5 qt5-default
 
 if [ $? -eq 0 ]; then
 	echo "----->Dependencies installed."
