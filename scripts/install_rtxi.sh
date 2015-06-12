4e74ebaadbb4aa97ef7e8b05c607f6a8d88821cd
Ansel <aesthapus@gmail.com>
2015-06-12 16:32:35 -0400

    removed --(enable|disable)-rtai flag because it doesn't exist

--- scripts/install_rtxi.sh
+++ scripts/install_rtxi.sh
@@ -41,11 +41,11 @@ echo "----->Please select your configuration and then press enter:"
 read kernel
 
 if [ $kernel -eq "1" ]; then
-	./configure --enable-xenomai --enable-analogy --disable-comedi --disable-rtai --disable-debug
+	./configure --enable-xenomai --enable-analogy --disable-comedi --disable-debug
 elif [ $kernel -eq "2" ]; then
-	./configure --enable-posix --disable-xenoami --disable-analogy --disable-comedi --disable-rtai --disable-debug
+	./configure --enable-posix --disable-xenoami --disable-analogy --disable-comedi --disable-debug
 elif [ $kernel -eq "3" ]; then
-	./configure --enable-posix --disable-xenomai --disable-analogy --disable-comedi --disable-rtai --enable-debug
+	./configure --enable-posix --disable-xenomai --disable-analogy --disable-comedi --enable-debug
 else
 	echo "Invalid configuration."
 	exit 1
