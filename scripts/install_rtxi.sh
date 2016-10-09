b187b8c2de9ff13acdcc4064feb257fa9a14c3c7
anselg <aesthapus@gmail.com>
2016-10-09 02:48:44 -0400

    fix typo in make -j2 -C

--- scripts/install_rtxi.sh
+++ scripts/install_rtxi.sh
@@ -107,7 +107,7 @@ for dir in ${MODS}/*; do
 	if [ -d "$dir" ]; then
 		make clean -C "$dir"
 		git -C "$dir" pull
-		make -j`nproc`-C "$dir"
+		make -j`nproc` -C "$dir"
 		sudo make install -C "$dir"
 	fi
 done
