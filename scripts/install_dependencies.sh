a6875c88836c6f4ded15d5403e1fefa5ecf4cc8f
yapatel <yogi@rtxi.org>
2017-07-19 21:07:33 -0400

    fixing error in ignore, changing install prefix

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -56,7 +56,7 @@ cd $DEPS
 mkdir "git" && cd "$_"
 git clone https://github.com/libgit2/libgit2.git && cd libgit2
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/libgit2 -DCURL=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ -DCURL=OFF
 cmake --build . --target install
 ldconfig
 echo "-----> libgit2 installed."
