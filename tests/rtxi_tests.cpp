cec195f7f0c0aac7086147bfb85f64885a54d01a
Ivan F. Valerio <valerioif@gmail.com>
2021-10-20 16:35:33 -0400

    added testing source files

---
+++ tests/rtxi_tests.cpp
@@ -0,0 +1,30 @@
+/*
+ 	 The Real-Time eXperiment Interface (RTXI)
+	 Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
+
+	 This program is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 3 of the License, or
+	 (at your option) any later version.
+
+	 This program is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+#define BOOST_TEST_MODULE rtxi_test 
+#include "boost/test/included/unit_test.hpp"
+
+
+BOOST_AUTO_TEST_CASE(first_test) 
+{
+  int i = 1;
+  BOOST_TEST(i); 
+  BOOST_TEST(i == 2); 
+}
+
