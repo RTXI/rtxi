2b6a4103b5d25f301c2148c78084351b47afa41c
Ivan F. Valerio <valerioif@gmail.com>
2022-05-31 14:06:52 -0400

    minor improvements to fifo tests

--- test/fifo_tests.cpp
+++ test/fifo_tests.cpp
@@ -40,7 +40,8 @@ TEST_F(FifoTest, roundtrip)
 {
   std::unique_ptr<RT::OS::Fifo> fifo;
   //char output[this->default_buffer_size];
-  std::string *output;
+  std::string *output = nullptr;
+  std::string *input = new std::string(this->default_message);
   int result = RT::OS::getFifo(fifo, this->default_buffer_size);
   ASSERT_EQ(result, 0);
   auto echo = [&fifo](size_t bufsize)
@@ -51,37 +52,44 @@ TEST_F(FifoTest, roundtrip)
     fifo->writeRT(&buf, bufsize);
     RT::OS::shutdown();
   };
-  fifo->write(&this->default_message, sizeof(std::string *));
+  fifo->write(&input, sizeof(std::string *));
   std::thread test_thread(echo, sizeof(std::string *));
   test_thread.join();
-  fifo->read(output, sizeof(std::string *), false);
+  fifo->read(&output, sizeof(std::string *), false);
   EXPECT_STREQ(this->default_message.c_str(), output->c_str());
+
+  // We should remove the double reference of output to avoid double free
+  output = nullptr;
+  delete input;
 }
 
 TEST_F(FifoTest, nonblocking)
 {
   std::unique_ptr<RT::OS::Fifo> fifo;
   //size_t size = this->default_buffer_size;
-  std::string *output;
+  std::string *input = new std::string(this->default_message);
+  std::string *output = nullptr;
   int result = RT::OS::getFifo(fifo, this->default_buffer_size);
   ASSERT_EQ(result, 0);
   auto test_task = std::make_unique<RT::OS::Task>();
   RT::OS::setPeriod(test_task, RT::OS::SECONDS_TO_NANOSECONDS);
-  auto sender = [this, &fifo, &test_task]()
+  auto sender = [this, &fifo, &test_task, &input]()
   {
     RT::OS::initiate();
     RT::OS::sleepTimestep(test_task);
-    fifo->writeRT(&(this->default_message), sizeof(std::string *));
+    fifo->writeRT(&input, sizeof(std::string *));
     RT::OS::shutdown();
   };
   int64_t start_time = RT::OS::getTime();
   std::thread sender_thread(sender);
-  fifo->read(output, sizeof(std::string *), false);
+  ssize_t read_bytes = fifo->read(&output, sizeof(std::string *), false);
   int64_t end_time = RT::OS::getTime();
   sender_thread.join();
   int64_t duration = end_time - start_time;
   // Should return immediately when in nonblocking mode. No message transferred
-  ASSERT_STRNE(output->c_str(), this->default_message.c_str());
+  ASSERT_EQ(read_bytes, 0);
   EXPECT_GE(test_task->period, duration);
+  output = nullptr;
+  delete input;
 }
 // NOLINTEND(*-avoid-c-arrays)
