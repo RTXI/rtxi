b1179c70c46f7f586ce7d725eab497b5e09b7b80
Ivan F. Valerio <valerioif@gmail.com>
2023-12-05 16:40:32 -0500

    added support for iir-filter, fir-window, mimic-signal, wave-maker, and noise-generator plugins during installation of RTXI

--- scripts/install_rtxi.sh
+++ scripts/install_rtxi.sh
@@ -56,14 +56,14 @@ mkdir ${MODS}
 cd ${MODS}
 
 # git clone https://github.com/RTXI/analysis-module.git
-# git clone https://github.com/RTXI/iir-filter.git
-# git clone https://github.com/RTXI/fir-window.git
+git clone https://github.com/RTXI/iir-filter.git
+git clone https://github.com/RTXI/fir-window.git
 # git clone https://github.com/RTXI/sync.git
-# git clone https://github.com/RTXI/mimic-signal.git
+git clone https://github.com/RTXI/mimic-signal.git
 git clone https://github.com/RTXI/signal-generator.git
 # git clone https://github.com/RTXI/ttl-pulses.git
-# git clone https://github.com/RTXI/wave-maker.git
-# git clone https://github.com/RTXI/noise-generator.git
+git clone https://github.com/RTXI/wave-maker.git
+git clone https://github.com/RTXI/noise-generator.git
 
 for dir in ${MODS}/*; do
 	if [ -d "$dir" ]; then
