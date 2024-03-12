f88682aaf680810ccd459bee1181b99c9cd9e136
Ivan F. Valerio <valerioif@gmail.com>
2024-03-12 11:19:42 -0400

    Updated psd estimation class logic and documentation

--- libs/dsp/psd_est.cpp
+++ libs/dsp/psd_est.cpp
@@ -2,65 +2,75 @@
 // file = psd_est.cpp
 //
 
+#include <cmath>
 #include <iostream>
-#include <stdlib.h>
 
 #include "psd_est.h"
 
-using namespace std;
+#include <gsl/gsl_sf_log.h>
 
 PsdEstimate::PsdEstimate(int num_samps, double samp_intvl)
+    : Num_Samps(num_samps)
+    , Delta_F(1.0 / samp_intvl / num_samps)
+    , Delta_T(samp_intvl)
 {
-  Num_Samps = num_samps;
-  Delta_F = 1.0 / samp_intvl / num_samps;
-  Delta_T = samp_intvl;
-  Psd_Est = new double[Num_Samps];
+  Psd_Est.resize(static_cast<size_t>(Num_Samps));
 }
-void
-PsdEstimate::DumpNumeric(ofstream* out_file, CpfskSpectrum* ref_spec)
-{
-  int samp_idx;
-  double freq, value, theor_val;
-  for (samp_idx = 0; samp_idx < Num_Samps / 2; samp_idx++) {
-    freq = samp_idx * Delta_F;
-    theor_val = ref_spec->GetPsdValue(freq);
-    value = Psd_Est[samp_idx];
-    (*out_file) << freq << ",  " << value << ", " << theor_val << std::endl;
-  }
-};
 
-void
-PsdEstimate::DumpNumeric(ofstream* out_file)
+void PsdEstimate::DumpNumeric(std::ofstream* out_stream,
+                              CpfskSpectrum* ref_spect)
 {
-  int samp_idx;
-  double freq, value;
-  for (samp_idx = 0; samp_idx < Num_Samps / 2; samp_idx++) {
-    freq = samp_idx * Delta_F;
+  double freq = NAN;
+  double value = NAN;
+  double theor_val = NAN;
+  for (size_t samp_idx = 0; samp_idx < static_cast<size_t>(Num_Samps) / 2;
+       samp_idx++)
+  {
+    freq = static_cast<double>(samp_idx) * Delta_F;
+    theor_val = ref_spect->GetPsdValue(freq);
     value = Psd_Est[samp_idx];
-    (*out_file) << freq << ",  " << value << std::endl;
+    (*out_stream) << freq << ",  " << value << ", " << theor_val << "\n";
   }
-};
+}
 
-void
-PsdEstimate::DumpDecibels(ofstream* out_file, CpfskSpectrum* ref_spec)
+void PsdEstimate::DumpNumeric(std::ofstream* out_stream)
 {
-  int samp_idx;
-  double freq, value, theor_val;
-  for (samp_idx = 0; samp_idx < Num_Samps / 2; samp_idx++) {
-    freq = samp_idx * Delta_F;
-    theor_val = 10.0 * log10(ref_spec->GetPsdValue(freq));
-    value = 10.0 * log10(Psd_Est[samp_idx]);
-    (*out_file) << freq << ",  " << value << ", " << theor_val << std::endl;
+  double freq = NAN;
+  double value = NAN;
+  for (size_t samp_idx = 0; samp_idx < static_cast<size_t>(Num_Samps) / 2;
+       samp_idx++)
+  {
+    freq = static_cast<double>(samp_idx) * Delta_F;
+    value = Psd_Est[samp_idx];
+    (*out_stream) << freq << ",  " << value << "\n";
   }
-};
-void
-PsdEstimate::DumpDecibels(ofstream* out_file)
+}
+
+void PsdEstimate::DumpDecibels(std::ofstream* out_stream,
+                               CpfskSpectrum* ref_spect)
 {
-  int samp_idx;
-  double freq, value;
-  for (samp_idx = 0; samp_idx < Num_Samps / 2; samp_idx++) {
-    freq = samp_idx * Delta_F;
-    value = 10.0 * log10(Psd_Est[samp_idx]);
-    (*out_file) << freq << ",  " << value << std::endl;
+  double freq = NAN;
+  double value = NAN;
+  double theor_val = NAN;
+  for (size_t samp_idx = 0; samp_idx < static_cast<size_t>(Num_Samps) / 2;
+       samp_idx++)
+  {
+    freq = static_cast<double>(samp_idx) * Delta_F;
+    theor_val = 10.0 * gsl_sf_log(ref_spect->GetPsdValue(freq));
+    value = 10.0 * gsl_sf_log(Psd_Est[samp_idx]);
+    (*out_stream) << freq << ",  " << value << ", " << theor_val << "\n";
   }
-};
+}
+
+void PsdEstimate::DumpDecibels(std::ofstream* out_stream)
+{
+  double freq = NAN;
+  double value = NAN;
+  for (size_t samp_idx = 0; samp_idx < static_cast<size_t>(Num_Samps) / 2;
+       samp_idx++)
+  {
+    freq = static_cast<double>(samp_idx) * Delta_F;
+    value = 10.0 * gsl_sf_log(Psd_Est[samp_idx]);
+    (*out_stream) << freq << ",  " << value << "\n";
+  }
+}
--- libs/dsp/psd_est.h
+++ libs/dsp/psd_est.h
@@ -3,25 +3,64 @@
 //
 #ifndef _PSD_EST_H_
 #define _PSD_EST_H_
-#include "complex.h"
-#include "fsk_spec.h"
-#include "thy_spec.h"
-#include <fstream>
 
+#include <fstream>
+#include <vector>
+
+#include "fsk_spec.h"
+
+/*!
+  * Power Spectrum Distribution (PSD) Estimate
+  *
+  * Base class for desigining power distribution estimators
+  */
 class PsdEstimate
 {
 public:
+  /*!
+    * Construct a PSD estimator
+    *
+    * \param num_samps The number of samples for this estimator
+    * \param samp_intvl The interval of the estimator
+    */
   PsdEstimate(int num_samps, double samp_intvl);
-  ~PsdEstimate(){};
+
+  /*!
+    * Dump numerical estimation and theoretical value to output stream
+    *
+    * \param out_stream Pointer to output stream where to dump data
+    * \param ref_spect Pointer to reference theoretical spectrum to dump
+    *                  along with the data
+    */
   void DumpNumeric(std::ofstream* out_stream, CpfskSpectrum* ref_spect);
+
+  /*!
+    * Dump numerical estimation to output stream
+    *
+    * \param out_stream Pointer to output stream where to dump data
+    */
   void DumpNumeric(std::ofstream* out_stream);
+
+  /*!
+    * Dump numerical estimation and theoretical value to output stream in Decibels
+    *
+    * \param out_stream Pointer to output stream where to dump data
+    * \param ref_spect Pointer to reference theoretical spectrum to dump
+    *                  along with the data
+    */
   void DumpDecibels(std::ofstream* out_stream, CpfskSpectrum* ref_spect);
+
+  /*!
+    * Dump numerical estimation to output stream in Decibels
+    *
+    * \param out_stream Pointer to output stream where to dump data
+    */
   void DumpDecibels(std::ofstream* out_stream);
 
-protected:
-  int Num_Samps;
-  double Delta_F;
-  double Delta_T;
-  double* Psd_Est;
+private:
+  int Num_Samps = 0;
+  double Delta_F = 0.0;
+  double Delta_T = 0.0;
+  std::vector<double> Psd_Est;
 };
 #endif
