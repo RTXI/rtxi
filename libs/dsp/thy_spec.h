d5f243f20af6e4083786f8e3efac8a695ec617a4
Ivan F. Valerio <valerioif@gmail.com>
2024-03-12 17:14:14 -0400

    updated theoretical spectrum code

--- libs/dsp/thy_spec.h
+++ libs/dsp/thy_spec.h
@@ -4,14 +4,26 @@
 #ifndef _THY_SPEC_H_
 #define _THY_SPEC_H_
 
+/*!
+ * Interface class describing a theoretical power spectrum.
+ */
 class TheoreticalSpectrum
 {
 public:
+  TheoreticalSpectrum() = default;
   TheoreticalSpectrum(const TheoreticalSpectrum&) = default;
   TheoreticalSpectrum(TheoreticalSpectrum&&) = delete;
   TheoreticalSpectrum& operator=(const TheoreticalSpectrum&) = default;
   TheoreticalSpectrum& operator=(TheoreticalSpectrum&&) = delete;
   virtual ~TheoreticalSpectrum() = default;
+
+  /*!
+    * Get Power Spectrum Distribution at given Frequency
+    *
+    * \param freq Frequency to retreive power spectrum value
+    *
+    * \return Power spectrum at given frequency
+    */
   virtual double GetPsdValue(double freq) = 0;
 };
 #endif
