1b58d73718ce2c7a4666b670f53cca8a288bad1a
Ivan F. Valerio <valerioif@gmail.com>
2024-04-12 15:16:26 -0400

    fixed connector bug where clicking on connection list would not properly update dropbox values

--- plugins/connector/connector.cpp
+++ plugins/connector/connector.cpp
@@ -278,7 +278,6 @@ void Connector::Panel::syncBlockInfo()
 
 void Connector::Panel::buildInputChannelList()
 {
-  auto prev_channel = inputChannel->currentData();
   inputChannel->clear();
   if (inputBlock->count() == 0) {
     return;
@@ -295,13 +294,11 @@ void Connector::Panel::buildInputChannelList()
     inputChannel->addItem(QString(block->getChannelName(IO::INPUT, i).c_str()),
                           QVariant::fromValue(i));
   }
-  inputChannel->setCurrentIndex(inputChannel->findData(prev_channel));
   updateConnectionButton();
 }
 
 void Connector::Panel::buildOutputChannelList()
 {
-  auto prev_channel = outputChannel->currentData();
   outputChannel->clear();
   if (outputBlock->count() == 0) {
     return;
@@ -321,7 +318,6 @@ void Connector::Panel::buildOutputChannelList()
     outputChannel->addItem(QString(block->getChannelName(direction, i).c_str()),
                            QVariant::fromValue(i));
   }
-  outputChannel->setCurrentIndex(outputChannel->findData(prev_channel));
   updateConnectionButton();
 }
 
@@ -375,10 +371,12 @@ void Connector::Panel::reverseHighlightConnectionBox(
       outputBlock->findData(QVariant::fromValue(connection.src)));
   outputFlag->setCurrentIndex(
       outputFlag->findData(QVariant::fromValue(connection.src_port_type)));
+  buildInputChannelList();
   outputChannel->setCurrentIndex(
       outputChannel->findData(QVariant::fromValue(connection.src_port)));
   inputBlock->setCurrentIndex(
       inputBlock->findData(QVariant::fromValue(connection.dest)));
+  buildOutputChannelList();
   inputChannel->setCurrentIndex(
       inputChannel->findData(QVariant::fromValue(connection.dest_port)));
   updateConnectionButton();
